8dc69e649e0659931216d8611662ee41463432b5
theshteves <kneisers@msu.edu>
2014-12-03 18:10:10 -0500

    learnyounode(5/13)

---
+++ learnyounode/test1.js
@@ -0,0 +1,18 @@
+/*---[HELLO WORLD]---(01/13)
+
+Write a program that prints the text "HELLO WORLD" to the console (stdout).
+
+*/
+
+// *sigh*
+console.log("HELLO WORLD");
+
+var youreDumb;
+
+do {
+    //Take a minute to think about what you just did.
+
+    if (youreDumb) //Stop coding.
+	return;    //Go home.
+
+} while (youreDumb)
