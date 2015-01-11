3156722d9c8559245504ab3b52959b5115bace1f
theshteves <kneisers@msu.edu>
2015-01-10 21:31:08 -0500

    learnyoucouchdb(1/4)

---
+++ learnyoucouchdb/test1.js
@@ -0,0 +1,9 @@
+/*---[COUCHDB SCHOOL]---(1/4)
+
+Welcome to learnyoucouchdb! Your first task is to install and start
+Apache CouchDB! You can download CouchDB from
+https://couchdb.apache.org/
+
+Did you know that CouchDB has excellent docs? You can finde them at
+http://docs.couchdb.org/
+*/
