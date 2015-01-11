25522056a4ba5f418c08eb0485fca3b39d66a4f1
theshteves <kneisers@msu.edu>
2015-01-10 23:10:38 -0500

    learnyoucouchdb(2/4)

---
+++ learnyoucouchdb/test2.js
@@ -0,0 +1,12 @@
+/*---[HELLO COUCHDB]---(2/4)
+
+Start Apache CouchDB and create a database named couchdbschool by
+using HTTP.
+
+After you created the database, please insert this document:
+
+    {
+      "_id": "robert",
+      "type": "human"
+    }
+*/
